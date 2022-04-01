3f695ff35d7b6088024637282be21c9881e24391
unknown <lindsay.kelly15@gmail.com>
2022-04-01 12:49:00 -0400

    middelware done

---
+++ api/auth/auth-middleware.js
@@ -0,0 +1,26 @@
+const Users = require('../auth/users-model');
+const { findBy } = require('../auth/users-model');
+
+async function checkUsernameExists(req, res, next) {
+    const user = await findBY({ username: req.body.username });
+    if(user) {
+        res.status(401).json({ message: 'username taken' });
+        return;
+    } else {
+        next();
+    }
+}
+
+function validatePost(req, res, next) {
+    if(!req.body.username || !req.body.password) {
+        res.status(401).json({ message: 'username and password required' });
+        return;
+    } else {
+        next();
+    }
+}
+
+module.exports = {
+    checkUsernameExists,
+    validatePost
+}
--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../secrets');
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+  if(!token) {
+    res.status(401).json({ message: 'token required' });
+    return;
+  } else {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+      if(err) {
+        res.status(401).json({ message: 'token invalid' });
+        return;
+      } else {
+        req.decodedToken = decodedToken;
+        next();
+      }
+    })
+  }
+
   /*
     IMPLEMENT
 
